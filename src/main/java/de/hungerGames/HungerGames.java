4a2d358e15917102cf01e04e20a1298cf929631f
Rael <raphael.keil@outlook.de>
2026-02-06 17:56:12 +0100

    Create HungerGames.java file

--- src/main/java/de/hungerGames/HungerGames.java
+++ src/main/java/de/hungerGames/HungerGames.java
@@ -1,7 +1,72 @@
 package de.hungerGames;
 
-public class HungerGames {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Hunger Games!");
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import de.hungerGames.listeners.PlayerDeathListener;
+import de.hungerGames.listeners.PlayerDamageListener;
+import de.hungerGames.commands.GameCommand;
+import de.hungerGames.managers.GameManager;
+import de.hungerGames.managers.ConfigManager;
+import de.hungerGames.managers.ScoreboardManager;
+
+public class HungerGames extends JavaPlugin {
+
+    private static HungerGames instance;
+    private GameManager gameManager;
+    private ConfigManager configManager;
+    private ScoreboardManager scoreboardManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        
+        // Konfiguration laden
+        configManager = new ConfigManager(this);
+        configManager.loadConfig();
+        
+        // Manager initialisieren
+        gameManager = new GameManager(this);
+        scoreboardManager = new ScoreboardManager(this);
+        
+        // Listener registrieren
+        registerListeners();
+        
+        // Commands registrieren
+        registerCommands();
+        
+        getLogger().info("§a✓ Hunger Games Plugin geladen!");
+    }
+
+    @Override
+    public void onDisable() {
+        if (gameManager != null) {
+            gameManager.stopGame();
+        }
+        getLogger().info("§c✗ Hunger Games Plugin deaktiviert!");
+    }
+
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new PlayerDeathListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerDamageListener(this), this);
+    }
+
+    private void registerCommands() {
+        getCommand("hg").setExecutor(new GameCommand(this));
+    }
+
+    public static HungerGames getInstance() {
+        return instance;
+    }
+
+    public GameManager getGameManager() {
+        return gameManager;
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public ScoreboardManager getScoreboardManager() {
+        return scoreboardManager;
     }
 }
